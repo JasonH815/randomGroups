d29c2c92adad3ed0f8665d76395ca4ce3837cc39
Jason <jasonhellwig@gmail.com>
2016-11-04 23:12:52 -0700

    Initial commit. Created Most of the UI to input students and thier table numbers.

---
+++ FrontEnd/app/scripts/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name randomGroupsApp
+ * @description
+ * # randomGroupsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('randomGroupsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngMessages',
+    //'ngRoute',
+    'ngSanitize',
+    //'ngTouch',
+    'ngMaterial',
+    'ui.router'
+  ])
+
+  // log ui-router errors
+  .run(function ($rootScope) {
+    $rootScope.$on("$stateChangeError", console.log.bind(console));
+  })
+
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    // redirect to home for any unmached url
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      });
+
+  }]);
---
+++ FrontEnd/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name randomGroupsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the randomGroupsApp
+ */
+angular.module('randomGroupsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
